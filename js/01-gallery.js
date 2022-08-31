fbedf75fea77871adc6056f58252004fd2b21f18
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-09-01 01:07:30 +0300

    finish task 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,3 +22,31 @@ function createGaleryItems(galleryItems) {
         `;
     }).join('');
 }
+
+const createLightboxInstance = (evt) => {
+    
+
+    const imgTarget = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${imgTarget}" width="800" height="600">
+    `);
+
+    instance.show(
+        document.addEventListener('keydown', (evt) => {
+            if (evt.key && evt.code === 'Escape') {
+                instance.close()
+            }
+        })
+    );
+};
+const onImgClick = (evt) => {
+    if (evt.target.nodeName !== 'IMG') return;
+    
+    createLightboxInstance(evt);
+    evt.preventDefault();  
+
+};
+
+gallery.addEventListener('click', onImgClick);
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGaleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGaleryItems(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+        `;
+    }).join('');
+};
+
+const lightboxSlider = new SimpleLightbox('.gallery a ', {
+    sourceAttr: 'href',
+    captions: true,
+    captionsData: 'title',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    loop: true,
+    overlayOpacity: 0.6,
+    close: false,
+    animationSpeed: 250,
+});
